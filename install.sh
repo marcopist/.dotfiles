8fabe07a48ae5905dbd1ca9bacd3f86e02a20130
Marco Pistilli <pistilli.mrc@gmail.com>
2024-05-12 09:26:46 +0100

    Install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+files=$(find ./home -type f | sed 's,^\./home/,,')
+
+for file in files; do
+    # Delete file in home folder with the same name if it exists
+    rm -rf "$HOME/$file"
+    # Create a symbolic link to the file in the home folder
+    ln -s "$PWD/home/$file" "$HOME/$file"
+done
+
+
